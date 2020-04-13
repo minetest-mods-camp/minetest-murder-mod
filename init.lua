aa6f932a8cd2d171ac30142aaabb3c70a2a05763
Giov4 <brancacciogiovanni1@gmail.com>
2020-04-13 20:02:21 +0000

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,9 +2,9 @@ dofile(minetest.get_modpath("murder") .. "/items.lua")
 dofile(minetest.get_modpath("murder") .. "/arena_lib/api.lua")
 dofile(minetest.get_modpath("murder") .. "/chatcmdbuilder.lua")
 dofile(minetest.get_modpath("murder") .. "/commands.lua")
+dofile(minetest.get_modpath("murder") .. "/arena_manager.lua")
 dofile(minetest.get_modpath("murder") .. "/roles_manager.lua")
 dofile(minetest.get_modpath("murder") .. "/kill_manager.lua")
-
 arena_lib.settings({
   prefix = "Murder > "
 })
