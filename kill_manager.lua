00b41d603511d31e9aee1f4a60dd87adaf85c15e
Giov4 <brancacciogiovanni1@gmail.com>
2020-04-13 20:01:10 +0000

    Update kill_manager.lua

--- kill_manager.lua
+++ kill_manager.lua
@@ -11,17 +11,13 @@ function(player)
 
         -- If someone kills the murderer the match finishes and victims win
         if arena.murderer == p_name then
-            for pl_name in pairs(arena.players) do
-                minetest.chat_send_player(pl_name, "The murderer has been killed by " .. arena.cop .. ", victims won the game!")
-            end
+            arena_lib.send_message_players_in_arena(arena, "The murderer has been killed by " .. arena.cop .. ", victims won the game!")
             arena_lib.end_arena(arena)
             
         -- If the murderer kills everyone the match finishes and he wins 
-        -- (the reason why it checks if there are only 2 players left instead of 1 is because when a player gets killed he isn't automatically kicked out from the arena)
-        elseif arena_lib.get_arena_players_count(arenaID) == 2 then
-            for pl_name in pairs(arena.players) do
-                minetest.chat_send_player(pl_name, arena.murderer .. " the murderer won!")
-            end
+        -- (the reason why it checks if there are only 2 players left instead of 1 is because when a player gets killed he doesn't automatically get kicked out from the arena)
+        elseif arena_lib.get_arena_players_count(arena) == 2 then
+            arena_lib.send_message_players_in_arena(arena, arena.murderer .. " the murderer won!")
             arena_lib.end_arena(arena)
 
         -- When somebody dies this sends a message to him
