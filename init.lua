ac1a073d57bc2da6233a5a9501ab002b9b897357
Giov4 <brancacciogiovanni1@gmail.com>
2020-04-14 20:56:41 +0000

    Adjusted files import order

--- init.lua
+++ init.lua
@@ -2,9 +2,13 @@ dofile(minetest.get_modpath("murder") .. "/items.lua")
 dofile(minetest.get_modpath("murder") .. "/arena_lib/api.lua")
 dofile(minetest.get_modpath("murder") .. "/chatcmdbuilder.lua")
 dofile(minetest.get_modpath("murder") .. "/commands.lua")
-dofile(minetest.get_modpath("murder") .. "/arena_manager.lua")
+dofile(minetest.get_modpath("murder") .. "/hud.lua")
 dofile(minetest.get_modpath("murder") .. "/roles_manager.lua")
 dofile(minetest.get_modpath("murder") .. "/kill_manager.lua")
+dofile(minetest.get_modpath("murder") .. "/arena_manager.lua")
+
+
+minetest.register_on_joinplayer(function (player) player:get_inventory():set_list("main", {}) end)
 
 arena_lib.settings({
   prefix = "Murder > "
