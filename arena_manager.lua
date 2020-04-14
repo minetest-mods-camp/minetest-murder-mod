59f7188786c6cce403d3a198dd4cf7496c246584
Giov4 <brancacciogiovanni1@gmail.com>
2020-04-14 21:38:23 +0000

    Moved timer in arena_manager.lua

--- arena_manager.lua
+++ arena_manager.lua
@@ -16,3 +16,22 @@ function arena_lib.on_end(_, players)
         remove_HUD(pl)
     end
 end
+
+
+-- Update the timer
+function timer(arena)
+    minetest.after(1,
+        function() 
+            arena.timer = arena.timer - 1 
+            for p_name, _ in pairs(arena.players) do
+                update_HUD(p_name, "timer_ID", arena.timer)
+            end
+            if arena.timer > 0 then timer(arena) 
+            else
+                if  arena.murderer ~= "" then
+                    arena_lib.send_message_players_in_arena(arena, "The time is over, victims won!")
+                end
+                arena_lib.end_arena(arena)
+            end
+        end)
+end
