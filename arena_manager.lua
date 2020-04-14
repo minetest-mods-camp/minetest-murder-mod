43a68d9fa21251ee68eca3d675caec8d5917b01b
Giov4 <brancacciogiovanni1@gmail.com>
2020-04-14 20:58:20 +0000

    Added HUD

--- arena_manager.lua
+++ arena_manager.lua
@@ -1,11 +1,26 @@
+
 function arena_lib.on_start(arena)
-    local match_duration = 180
+    match_duration = 180
+    arena.timer = match_duration
+    timer(arena)
+    
     minetest.after(match_duration,
-    function ()
-        if arena_lib.get_arena_players_count(arena) > 1 then
-            arena_lib.send_message_players_in_arena(arena, "The time is over, victims won!")
-        end
-        arena_lib.end_arena(arena)
-    end)  
+        function ()
+            if arena_lib.get_arena_players_count(arena) > 1 then
+                arena_lib.send_message_players_in_arena(arena, "The time is over, victims won!")
+            end
+            arena_lib.end_arena(arena)
+        end)  
+        
     manage_roles(arena)
+    for pl in pairs(arena.players) do
+        generate_HUD(arena, pl)
+    end
+end
+
+
+function arena_lib.on_end(_, players)
+    for pl, _ in pairs(players) do
+        remove_HUD(pl)
+    end
 end
