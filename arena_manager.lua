e9be3901f9b2524e1d21f4697503060055b4208f
Giov4 <brancacciogiovanni1@gmail.com>
2020-04-14 21:14:10 +0000

    Moved timer in HUD.lua

--- arena_manager.lua
+++ arena_manager.lua
@@ -2,15 +2,7 @@
 function arena_lib.on_start(arena)
     match_duration = 180
     arena.timer = match_duration
-    timer(arena)
-    
-    minetest.after(match_duration,
-        function ()
-            if arena_lib.get_arena_players_count(arena) > 1 then
-                arena_lib.send_message_players_in_arena(arena, "The time is over, victims won!")
-            end
-            arena_lib.end_arena(arena)
-        end)  
+    timer(arena)  
         
     manage_roles(arena)
     for pl in pairs(arena.players) do
