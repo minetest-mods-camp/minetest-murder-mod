1e6777876dbc08e14ac5d68e37c6bbbd97cde396
Giov4 <brancacciogiovanni1@gmail.com>
2020-04-15 00:17:40 +0000

    Documented init.lua

--- init.lua
+++ init.lua
@@ -7,12 +7,13 @@ dofile(minetest.get_modpath("murder") .. "/roles_manager.lua")
 dofile(minetest.get_modpath("murder") .. "/kill_manager.lua")
 dofile(minetest.get_modpath("murder") .. "/arena_manager.lua")
 
-
+-- Delete player's inventory when he joins
 minetest.register_on_joinplayer(function (player) player:get_inventory():set_list("main", {}) end)
 
 arena_lib.settings({
   prefix = "Murder > "
 })
+
 
 minetest.register_privilege("murder_admin", {
     
