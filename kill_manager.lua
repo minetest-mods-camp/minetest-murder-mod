b0a3371cf4dae0adc05cb66d96f3900829167bec
Giov4 <brancacciogiovanni1@gmail.com>
2020-05-01 16:19:52 +0000

    Now kill_manager uses on_death callback and the new kick function

--- kill_manager.lua
+++ kill_manager.lua
@@ -1,33 +1,26 @@
 -- THIS HANDLES IN-GAME KILLS -- 
 
-minetest.register_on_dieplayer(
-function(player)
-    local p_name = player:get_player_name()
+arena_lib.on_death("murder", function(arena, p_name)
+    
+    -- If the player is in an arena
+    minetest.after(1, function()
+        -- If someone kills the murderer the match finishes and victims win
+        if arena.murderer == p_name then
+            arena_lib.send_message_players_in_arena(arena, "The murderer has been killed by " .. minetest.colorize("#f9a31b", arena.cop) .. ", victims won the game!")
+            arena.timer = 0
+            arena.murderer = ""
 
-    if arena_lib.is_player_in_arena(p_name) then
-        local arenaID = arena_lib.get_arenaID_by_player(p_name)
-        local arena = arena_lib.mods["murder"].arenas[arenaID]
+        -- If the murderer kills everyone the match finishes and he wins 
+        -- (the reason why it checks if there are only 2 players left instead of 1 is because when a player gets killed he doesn't automatically get kicked out from the arena)
+        elseif arena_lib.get_arena_players_count(arena) == 2 then
+            arena_lib.send_message_players_in_arena(arena, minetest.colorize("#f9a31b", arena.murderer) .. " the murderer won!")
+            arena.murderer = ""
+            arena.timer = 0
 
-        -- If the player is in an arena
-        minetest.after(1, function()
-            -- If someone kills the murderer the match finishes and victims win
-            if arena.murderer == p_name then
-                arena_lib.send_message_players_in_arena(arena, "The murderer has been killed by " .. minetest.colorize("#f9a31b", arena.cop) .. ", victims won the game!")
-                arena.timer = 0
-                arena.murderer = ""
-
-            -- If the murderer kills everyone the match finishes and he wins 
-            -- (the reason why it checks if there are only 2 players left instead of 1 is because when a player gets killed he doesn't automatically get kicked out from the arena)
-            elseif arena_lib.get_arena_players_count(arena) == 2 then
-                arena_lib.send_message_players_in_arena(arena, minetest.colorize("#f9a31b", arena.murderer) .. " the murderer won!")
-                arena.murderer = ""
-                arena.timer = 0
-
-            -- When somebody dies this sends a message to him
-            else
-                arena_lib.remove_player_from_arena(p_name)
-                minetest.chat_send_player(p_name, "You've been killed!")
-            end
-        end)
-    end
+        -- When somebody dies this sends a message to him
+        else
+            arena_lib.remove_player_from_arena(p_name, true)
+            minetest.chat_send_player(p_name, "You've been killed!")
+        end
+    end)
 end)
