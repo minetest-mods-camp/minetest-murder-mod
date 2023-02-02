1ba4596eb38f0d8544ea58243cbd18e627d258ea
Zughy <4279489-marco_a@users.noreply.gitlab.com>
2023-02-02 13:15:28 +0100

    Remove chatcmdbuilder (already bundled with arena_lib) and redundant commands

--- commands.lua
+++ commands.lua
@@ -1,5 +1,5 @@
 -- Registering the murder_admin privilege.
-minetest.register_privilege("murder_admin", {  
+minetest.register_privilege("murder_admin", {
     description = murder.T("It allows you to use /murder")
 })
 
@@ -9,69 +9,6 @@ ChatCmdBuilder.new("murder", function(cmd)
 
     cmd:sub("tutorial", function(name)
         minetest.chat_send_player(name, "See the TUTORIAL.txt file in the mod folder.")
-    end)
-
-
-
-    cmd:sub("create :arena", function(name, arena_name)
-        arena_lib.create_arena(name, "murder", arena_name)
-    end)
-
-
-
-    cmd:sub("create :arena :minplayers:int :maxplayers:int", function(name, arena_name, min_players, max_players)
-        arena_lib.create_arena(name, "murder", arena_name, min_players, max_players)
-    end)
-
-
-
-    cmd:sub("remove :arena", function(name, arena_name)
-        arena_lib.remove_arena(name, "murder", arena_name)
-    end)
-
-    
-    
-    -- list of the arenas
-    cmd:sub("list", function(name)
-        arena_lib.print_arenas(name, "murder")
-    end)
-
-
-
-    cmd:sub("info :arena", function(name, arena_name)
-        arena_lib.print_arena_info(name, "murder", arena_name)
-    end)
-
-
-
-    -- This sets the spawns using the player position.
-    cmd:sub("setspawn :arena", function(name, arena)
-        arena_lib.set_spawner(name, "murder", arena)
-    end)
-
-
-
-    -- This sets the arena sign.
-    cmd:sub("setsign :arena", function(sender, arena)
-        arena_lib.set_sign(sender, nil, nil, "murder", arena)
-    end)
-
-
-    
-    cmd:sub("edit :arena", function(sender, arena)
-        arena_lib.enter_editor(sender, "murder", arena)
-    end)
-
-
-
-    cmd:sub("enable :arena", function(name, arena)
-        arena_lib.enable_arena(name, "murder", arena)
-    end)
-
-
-
-    cmd:sub("disable :arena", function(name, arena)
-        arena_lib.disable_arena(name, "murder", arena)
     end)
 
 
@@ -89,16 +26,16 @@ ChatCmdBuilder.new("murder", function(cmd)
 
 end, {
   description = [[
-     
+
     ADMIN COMMANDS
     (Use /help murder to read it all)
 
     Use this to configure your arena:
     - tutorial
     - create <arena name> [min players] [max players]
-    - edit <arena name> 
+    - edit <arena name>
     - enable <arena name>
-    
+
     Other commands:
     - list
     - info <arena name>
